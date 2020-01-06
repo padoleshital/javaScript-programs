58bdbe29b620a470060f52bc4b0cccb53e19569a
padoleshital <padoleshital@github.com>
2020-01-06 18:07:32 +0530

    basic of  php
    it contain info about comment ,expression,string literal

---
+++ expression.php
@@ -0,0 +1,11 @@
+
+<?php
+    echo"a:[".(18==8)."]<br>";
+    echo"b:[".(14>6)."]<br>";
+    echo"c:[".(14<6)."]<br>";
+    echo"d:[".(18==18)."]<br>";
+    echo "[".true."]";  // 1
+    echo "[".false."]"; // null
+    /* epression b: and d: are true they show 1 value
+     epression a: and c: are false they show null(nothing) value */
+?>
---
+++ string_literal.php
@@ -0,0 +1,12 @@
+<?php
+$myname ="shital";  //declare variable
+$age =25;
+echo "a:".$myname."<br>";   //Allowed -variable string literal
+echo "b:".$age."<br>";      //Allowed -variable numeric literal
+echo "c:"."hello shital"."<br>";    //Allowed  string literal
+echo "e:".FALSE."<br>";     // constant literal
+echo "f:".true."<br>";      //constant literal
+//echo "a:".789."<br>" // allowd number literal
+
+
+?>
