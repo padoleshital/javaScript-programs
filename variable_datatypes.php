257a8088e4d128535ff1ad2bbeb11d57999d6fda
padoleshital <padoleshital@github.com>
2020-01-10 20:28:15 +0530

    datatypes
     declartion of variable and datatypes

---
+++ variable_datatypes.php
@@ -0,0 +1,24 @@
+<?php
+// variable assignments/datatypes in php
+
+$myint=1;   //interger
+echo"value inside myint:$myint";
+echo"<br>";
+$myfloat=15.9;  //float/double
+echo"value inside myfloat:$myfloat";
+echo"<br>";
+$mystring="shital"; //string
+echo"value inside mystring:$mystring";
+echo"<br>";
+$string='shital';   //string
+echo"value inside string:$string";
+echo"<br>";
+$myarray=array("shital","kavita","rani");   //array
+print_r($myarray);
+echo"<br>";
+$bool=TRUE;
+echo"value inside boolean variable :$bool";
+
+
+
+?>
