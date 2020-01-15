0e120261617b8419c53238183e4f347c2d69b7d2
padoleshital <padoleshital@github.com>
2020-01-15 17:46:17 +0530

    array traversal
    array travaersal using for loop.

---
+++ Arrays/array_traversal.php
@@ -0,0 +1,13 @@
+<?php
+$company=array("microsoft","quick","heal","avaya");
+$arrlength=count($company);
+//1. using for loop
+
+for($x=0; $x < $arrlength; $x++);
+{
+    echo $x;
+    echo"<br>";
+}
+
+
+?>
