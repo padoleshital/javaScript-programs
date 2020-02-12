f29e435b7fdaf3830809ffc95eaca36eae6c832d
padoleshital <padoleshital@github.com>
2020-02-12 16:50:36 +0530

    username_password validation
    validation using javascript alert box

---
+++ upload.php
@@ -0,0 +1,12 @@
+<?php
+<html>
+<head>
+    <title>PHP form upload</title>
+</head>
+<body>
+    <form method='post' action='upload1.php' enctype='multipart/form-data'>
+    </form>
+</body>
+</html>
+
+?>
